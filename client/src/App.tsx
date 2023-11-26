73c20cb8819c8cea1e6b32bafe8d0a04055b8a93
ChaoticGood <colete_nico@yahoo.com>
2023-11-26 12:28:24 +0800

    Configured to use Material UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
 
+import Icon from '@mui/material/Icon';
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Icon>star</Icon>
     </>
   )
 }
