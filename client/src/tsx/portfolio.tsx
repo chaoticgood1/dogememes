773412c0ce5a45a12328be32819cce4732d4e0f0
ChaoticGood <colete_nico@yahoo.com>
2024-01-07 21:34:58 +0800

    Save point

--- client/src/tsx/portfolio.tsx
+++ client/src/tsx/portfolio.tsx
@@ -1,6 +1,87 @@
 
 
 export default function Portfolio() {
-  return <h1>Portfolio</h1>;
+  return <div>
+    <section>
+      Sept 2020 - Oct 2023
+      3D Game Dev
+
+      <section>
+        Programming language: Rust
+        Tools: Bevy Engine, WASM, Cargo, PostgreSQL, WGSL
+      </section>
+
+
+
+      {/* 
+        Show demo about ironverse
+        Discuss about the technologies and tools being used
+        What I have learned  
+      */}
+    </section>
+    <hr></hr>
+    <div>
+      April 2017 - Sept 2020
+      Web Dev
+      Katanatechworks Inc.
+      <hr></hr>
+      In-charge of creating the front-end Wordpress and few Magento 2 back-end
+      <hr></hr>
+      Created custom plugin to convert excel to csv to use WooCommerce Product CSV Importer and Exporter
+      Implemented front-end of three websites
+
+      Tools: Wordpress, Magento 2, PHP, Javascript, CSS, HTML, MySQL Cypress
+      {/* 
+        Show demo and say what parts I participated with:
+          - Put dates
+          - Layout change that are not present now
+          pbdionisio wordpress
+          Muse square wordpress
+          TCC Inc.
+
+        Discuss about the technologies and tools being used
+        What I have learned  
+      */}
+    </div>
+    <hr></hr>
+    <div>
+      July 2016 - March 2017
+      Java Dev
+      Hilsoft Inc.
+      https://www.hilsoftinc.com/
+      <hr></hr>
+      Worked with ERP Research and Development which involves several ERP modules
+
+      <hr></hr>
+      Tools: Java, CSS, HTML, JEJB, Struts, MySQL, JSP, iReport, Apache Ant
+
+      {/* 
+        No demo
+        Discuss about the technologies and tools being used
+        What I have learned  
+      */}
+
+    </div>
+    <hr></hr>
+    <div>
+      Sept 2014 - July 2016
+      Indigo.Entertainment Phils. Inc.
+      2D Game Dev
+      <hr></hr>
+
+      {/* 
+        Show demo and parts I was in charge
+        Discuss about the technologies and tools being used
+        What I have learned 
+        Say the clients
+      */}
+    </div>
+
+    <div>
+      Pet Projects
+    </div>
+
+
+  </div>;
 }
 
