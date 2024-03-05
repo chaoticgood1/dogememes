82ce0c83fdaedf87e4c3fc9c5357ee668d6c69a1
ChaoticGood <colete_nico@yahoo.com>
2024-03-05 13:41:39 +0800

    Updated portfolio page

--- client/src/tsx/portfolio.tsx
+++ client/src/tsx/portfolio.tsx
@@ -3,86 +3,90 @@
 export default function Portfolio() {
   return <div className='timeline'>
     <div className="timeline-block">
-      
       <div className='time'>
         <span>Sept 2020 - Oct 2023</span>
+        <a href="https://github.com/gamedevalice">GameDevAlice</a>
         <span>3D Game Dev</span>
       </div>
       <div className="divider">
         <div className="entry-dot"></div>
       </div>
-      <div>
-        Programming language: Rust
-        Tools: Bevy Engine, WASM, Cargo, PostgreSQL, WGSL
+      <div className="description">
+        <div>
+          <a href="https://stoicmemes.com/ironverse">Ironverse</a>
+        </div>
+        Have built lots of proprietary prototypes, the link above is the only open-source we created using WASM
+        <br></br>
+        Programming language: Rust, Javascript, HTML
+        Tools: Bevy Engine, WASM, Cargo, PostgreSQL, WGSL, Tokio, Multithreading, Game Physics, Voxel, Shader
       </div>
-      
-
-      {/* 
-        Show demo about ironverse
-        Discuss about the technologies and tools being used
-        What I have learned  
-      */}
     </div>
-    <hr></hr>
+
     <div className="timeline-block">
-      April 2017 - Sept 2020
-      Web Dev
-      Katanatechworks Inc.
-      <hr></hr>
-      In-charge of creating the front-end Wordpress and few Magento 2 back-end
-      <hr></hr>
-      Created custom plugin to convert excel to csv to use WooCommerce Product CSV Importer and Exporter
-      Implemented front-end of three websites
-
-      Tools: Wordpress, Magento 2, PHP, Javascript, CSS, HTML, MySQL Cypress
-      {/* 
-        Show demo and say what parts I participated with:
-          - Put dates
-          - Layout change that are not present now
-          pbdionisio wordpress
-          Muse square wordpress
-          TCC Inc.
-
-        Discuss about the technologies and tools being used
-        What I have learned  
-      */}
-    </div>
-    <hr></hr>
-    <div>
-      July 2016 - March 2017
-      Java Dev
-      Hilsoft Inc.
-      https://www.hilsoftinc.com/
-      <hr></hr>
-      Worked with ERP Research and Development which involves several ERP modules
-
-      <hr></hr>
-      Tools: Java, CSS, HTML, JEJB, Struts, MySQL, JSP, iReport, Apache Ant
-
-      {/* 
-        No demo
-        Discuss about the technologies and tools being used
-        What I have learned  
-      */}
-
-    </div>
-    <hr></hr>
-    <div>
-      Sept 2014 - July 2016
-      Indigo.Entertainment Phils. Inc.
-      2D Game Dev
-      <hr></hr>
-
-      {/* 
-        Show demo and parts I was in charge
-        Discuss about the technologies and tools being used
-        What I have learned 
-        Say the clients
-      */}
+      <div className='time'>
+        <span>April 2017 - Sept 2020</span>
+        <a href="https://www.katanatechworks.com/">Katana Techworks Inc.</a>
+        <span>Web Dev</span>
+      </div>
+      <div className="divider">
+        <div className="entry-dot"></div>
+      </div>
+      <div className="description">
+        <div>
+          <a href="https://musesquare.com/">Muse Square</a>
+          , <a href="https://www.pbdionisio.com/shop/">PB Dionisio</a>
+          , <a href="https://tccinc.com/">TCC Inc.</a>
+        </div>
+        I learned a lot here about web development.
+        <br></br>
+        Programming language: PHP, Javascript, CSS, HTML, JQuery
+        Tools: MySQL/MariaDB, Wordpress, Magento 2, Postman, Laravel, Git, Cypress, Visual Studio Code, Ubuntu, Windows 7/10, GitExtension, Google Analytics
+      </div>
     </div>
 
-    <div>
-      Pet Projects
+    <div className="timeline-block">
+      <div className='time'>
+        <span>July 2016 - March 2017</span>
+        <a href="https://www.hilsoftinc.com/">Hilsoft Inc.</a>
+        <span>Web Dev</span>
+      </div>
+      <div className="divider">
+        <div className="entry-dot"></div>
+      </div>
+      <div className="description">
+        We worked with CBTL and CWC
+        <br></br>
+        Programming language: Java, CSS, HTML
+        Tools: JEJB, Struts, MySQL, JSP, iReport, Apache Ant
+      </div>
+    </div>
+
+    <div className="timeline-block">
+      <div className='time'>
+        <span>April 2017 - Sept 2020</span>
+        <a href="https://www.katanatechworks.com/">Katana Techworks Inc.</a>
+        <span>Web Dev</span>
+      </div>
+      <div className="divider">
+        <div className="entry-dot"></div>
+      </div>
+      <div className="description">
+        <div>
+          <a href="https://www.numuki.com/game/tmnt-deck-d-out/">TMNT Deckd Out</a>
+          , <a href="https://www.boomerangtv.co.uk/games/mr-bean-skidding">Mr. Bean Skidding</a>
+          , <a href="http://www.qgames.org/game/alvin-and-the-chipmunks-taking-center-stage.html">Alvin and the Chipmunks</a>
+          , <a href="https://www.numuki.com/game/nick-dance-machine/">Nick Dance Machine</a>
+          , <a href="https://www.numuki.com/game/fop-wishing-101/">Fairly Odd Parents</a>
+          , <a href="https://www.numuki.com/game/pgbc-game-lad-blitz/">PGBC Game Lad Blitz</a>
+        </div>
+        I started my career as a Game Developer
+        <br></br>
+        Programming language: Haxe, Java
+        Tools: HTML, Flash, Flambe, OpenFl, Mercurial, FlashDevelop 5
+      </div>
+    </div>
+
+    <div className="timeline-block">
     </div>
 
 
