ea3425cf627f355ddd464505d7421a716de2f9e8
Doge <colete_nico@yahoo.com>
2024-12-09 01:57:22 +0800

    Refactored portfolio

---
+++ client/src/tsx/portfolio/descriptions.tsx
@@ -0,0 +1,149 @@
+import React from 'react';
+import styles from "./Index.module.scss";
+import { Box, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+
+type Props = {
+  period: string,
+  company: Map<string, string>,
+  position: string,
+  projects: Map<string, string>,
+  summaries: string[],
+  common: string[],
+  backend: string[],
+  frontend: string[],
+};
+
+const Descriptions: React.FC<Props> = ({
+  period, 
+  company,
+  position, 
+  projects, 
+  summaries, 
+  common, 
+  backend, 
+  frontend
+}) => {
+  return (
+    <Box className={styles.timeline}>
+      <Company period={period} company={company} position={position} />
+      
+      <Box className={styles.divider}>
+        <Box className={styles.entryDot}></Box>
+      </Box>
+      <Box className={styles.description}>
+        <Box sx={{ display: "flex"}}>
+          <Projects projects={projects} />
+
+          <List>
+            <Typography>
+              Summaries:
+            </Typography>
+            {summaries.map((tech, index) => (
+              <ListItem key={index} sx={{ padding: "0px", }}>
+                <ListItemText primary={`${tech}`} sx={{ margin: "0px", pl: 2}}/>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+
+        <Box sx={{ display: "flex"}}>
+          <List className={styles.list}>
+            <Typography>
+              Common:
+            </Typography>
+            {common.map((tech, index) => (
+              <ListItem key={index} sx={{ padding: "0px", }}>
+                <ListItemIcon sx={{ minWidth: "20px" }}>
+                  <FiberManualRecordIcon sx={{ fontSize: "10px" }} color="primary" />
+                </ListItemIcon>
+                <ListItemText primary={tech} sx={{ margin: "0px", }}/>
+              </ListItem>
+            ))}
+          </List>
+          <List className={styles.list}>
+            <Typography>
+              Backend:
+            </Typography>
+            {backend.map((tech, index) => (
+              <ListItem key={index} sx={{ padding: "0px", }}>
+                <ListItemIcon sx={{ minWidth: "20px" }}>
+                  <FiberManualRecordIcon sx={{ fontSize: "10px" }} color="primary" />
+                </ListItemIcon>
+                <ListItemText primary={tech} sx={{ margin: "0px", }}/>
+              </ListItem>
+            ))}
+          </List>
+          <List className={styles.list}>
+            <Typography>
+              Frontend:
+            </Typography>
+            {frontend.map((tech, index) => (
+              <ListItem key={index} sx={{ padding: "0px", }}>
+                <ListItemIcon sx={{ minWidth: "20px" }}>
+                  <FiberManualRecordIcon sx={{ fontSize: "10px" }} color="primary" />
+                </ListItemIcon>
+                <ListItemText primary={tech} sx={{ margin: "0px", }} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        
+      </Box>
+    </Box>
+  )
+}
+
+type CompanyProps = {
+  period: string,
+  company: Map<string, string>,
+  position: string
+}
+
+const Company: React.FC<CompanyProps> = ({ period, company, position }) => {
+  return (
+    <Box>
+      <Typography>{period}</Typography>
+      {Array.from(company).map(([key, value], index) => (
+        <Box key={index}>
+          {value === "" ? (
+            <Typography>{key}</Typography>
+          ) : (
+            <a href={value} target="_blank">{key}</a>
+          )}
+        </Box>
+      ))}
+      
+      <Typography>{position}</Typography>
+    </Box>
+  )
+}
+
+
+type ProjectsProps = {
+  projects: Map<string, string>
+}
+
+const Projects: React.FC<ProjectsProps> = ({projects}) => {
+  return (
+    <List sx={{width: "210px", minWidth: "210px"}}>
+      <Typography>
+        Projects:
+      </Typography>
+      {Array.from(projects).map(([key, value], index) => (
+        <ListItem key={index} sx={{ padding: "0px", }}>
+          <ListItemIcon sx={{ minWidth: "20px" }}>
+            <FiberManualRecordIcon sx={{ fontSize: "10px" }} color="primary" />
+          </ListItemIcon>
+          {value === "" ? (
+            <ListItemText primary={key} sx={{ margin: "0px", }}/>
+          ) : (
+            <a href={value} target="_blank">{key}</a>
+          )}
+        </ListItem>
+      ))}
+    </List>
+  )
+}
+
+export default Descriptions;
--- client/src/tsx/portfolio/portfolio.tsx
+++ client/src/tsx/portfolio/portfolio.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from "./Index.module.scss";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import Descriptions from './descriptions';
 
 export default function Portfolio() {
   const navigate = useNavigate()
@@ -11,231 +12,227 @@ export default function Portfolio() {
   return <div className='timeline'>
     <IamTech />
     <GlobalX />
-
-    <div className="timeline-block">
-      <div className='time'>
-        <span>Sept 2020 - Oct 2023</span>
-        <a href="https://github.com/gamedevalice" target="_blank">GameDevAlice</a>
-        <span>3D Game Dev</span>
-      </div>
-      <div className="divider">
-        <div className="entry-dot"></div>
-      </div>
-      <div className="description">
-        <div>
-          <a href="/ironverse" target="_blank">Ironverse</a>
-        </div>
-        Have built lots of proprietary prototypes, the link above is the only open-source we created using WASM.
-        <br></br>
-        Programming languages: Rust, Javascript
-        <br></br>
-        Tools: Bevy Engine, WASM, Cargo, PostgreSQL, WGSL, Tokio, Multithreading, Game Physics, Voxel, Shader
-      </div>
-    </div>
-
-    <div className="timeline-block">
-      <div className='time'>
-        <span>April 2017 - Sept 2020</span>
-        <a href="https://www.katanatechworks.com/" target="_blank">Katana Techworks Inc.</a>
-        <span>Web Dev</span>
-      </div>
-      <div className="divider">
-        <div className="entry-dot"></div>
-      </div>
-      <div className="description">
-        <div>
-          <a href="https://musesquare.com/" target="_blank">Muse Square</a>
-          , <a href="https://www.pbdionisio.com/shop/" target="_blank">PB Dionisio</a>
-          , <a href="https://tccinc.com/" target="_blank">TCC Inc.</a>
-        </div>
-        I learned a lot here about web development.
-        <br></br>
-        Programming languages: PHP, Javascript
-        <br></br>
-        Tools: MySQL/MariaDB, Wordpress, Magento 2, Postman, Laravel, Git, Cypress, Visual Studio Code, Ubuntu, Windows 7/10, GitExtension, Google Analytics
-      </div>
-    </div>
-
-    <div className="timeline-block">
-      <div className='time'>
-        <span>July 2016 - March 2017</span>
-        <a href="https://www.hilsoftinc.com/" target="_blank">Hilsoft Inc.</a>
-        <span>Web Dev</span>
-      </div>
-      <div className="divider">
-        <div className="entry-dot"></div>
-      </div>
-      <div className="description">
-        We worked with CBTL and CWC.
-        <br></br>
-        Programming languages: Java
-        <br></br>
-        Tools: JEJB, Struts, MySQL, JSP, iReport, Apache Ant
-      </div>
-    </div>
-
-    <div className="timeline-block">
-      <div className='time'>
-        <span>Sept 2014 - July 2016</span>
-        <a href="https://ph.indeed.com/cmp/Indigo-Entertainment" target="_blank">Indigo.Entertainment Phils. Inc.</a>
-        <span>Web Dev</span>
-      </div>
-      <div className="divider">
-        <div className="entry-dot"></div>
-      </div>
-      <div className="description">
-        <div>
-          <a href="https://www.numuki.com/game/tmnt-deck-d-out/" target="_blank">TMNT Deckd Out</a>
-          , <a href="https://www.boomerangtv.co.uk/games/mr-bean-skidding" target="_blank">Mr. Bean Skidding</a>
-          , <a href="http://www.qgames.org/game/alvin-and-the-chipmunks-taking-center-stage.html" target="_blank">Alvin and the Chipmunks</a>
-          , <a href="https://www.numuki.com/game/nick-dance-machine/" target="_blank">Nick Dance Machine</a>
-          , <a href="https://www.numuki.com/game/fop-wishing-101/" target="_blank">Fairly Odd Parents</a>
-          , <a href="https://www.numuki.com/game/pgbc-game-lad-blitz/" target="_blank">PGBC Game Lad Blitz</a>
-        </div>
-        I started my career as a Game Developer, we worked for Cartoon Network and Nickelodeon.
-        <br></br>
-        Programming languages: Haxe, Java
-        <br></br>
-        Tools: Flash, Flambe, OpenFl, Mercurial, FlashDevelop 5
-      </div>
-    </div>
-
-    <div className="timeline-block">
-    </div>
+    <GameDevAlice />
+    <Katanatechworks />
+    <Hilsoft />
+    <Indigo />
   </div>;
 }
 
-function IamTech() {
-  const projects = [
-    "Kingfisher", "HorseRace", "MegaPerya", "Atlas"
-  ]
+const IamTech: React.FC = () => {
+  const period = "July 2024 - Dec 2024"
+  const company = new Map([["IamTech", ""]])
+  const position = "CTO"
+  const projects = new Map([
+    ["Kingfisher", ""], 
+    ["HorseRace", ""], 
+    ["MegaPerya", ""], 
+    ["Atlas", ""], 
+  ])
+  
   const summaries = [
     "Online betting boat race",
     "Online betting horse race",
     "Online betting for color game",
     "Created an EKYC prototype to validate players",
   ]
-  const backend = [
-    "NodeJS", "Fastify", "Uecs", "SigePay", "MySQL", "SQLite", "PostgreSQL",
-  ]
   const common = [
     "Typescript", "Jest", "Supertest"
+  ]
+  const backend = [
+    "NodeJS", "Fastify", "Uecs", "SigePay", "MySQL", "SQLite", "PostgreSQL",
   ]
   const frontend = [
     "ReactJS", "Mui", "Uecs", "Zustand", "Phaser"
   ]
   return (
-    <Box className={styles.timeline}>
-      <Box>
-        <Typography>July 2024 - Dec 2024</Typography>
-        <Typography>IamTech</Typography>
-        <Typography>CTO</Typography>
-      </Box>
-      <Box className={styles.divider}>
-        <Box className={styles.entryDot}></Box>
-      </Box>
-      <Box className={styles.description}>
-        <Box sx={{ display: "flex"}}>
-          <List sx={{paddingRight: "30px"}}>
-            <Typography>
-              Projects:
-            </Typography>
-            {projects.map((tech, index) => (
-              <ListItem key={index} sx={{ padding: "0px", height: "20px" }}>
-                <ListItemIcon sx={{ minWidth: "20px" }}>
-                  <FiberManualRecordIcon sx={{ fontSize: "10px" }} color="primary" />
-                </ListItemIcon>
-                <ListItemText primary={tech} />
-              </ListItem>
-            ))}
-          </List>
-
-          <List>
-            <Typography>
-              Summaries:
-            </Typography>
-            {summaries.map((tech, index) => (
-              <ListItem key={index} sx={{ padding: "0px", height: "20px" }}>
-                <ListItemText primary={tech} />
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-
-        <Box sx={{ display: "flex"}}>
-          <List className={styles.list}>
-            <Typography>
-              Common:
-            </Typography>
-            {common.map((tech, index) => (
-              <ListItem key={index} sx={{ padding: "0px", height: "20px" }}>
-                <ListItemIcon sx={{ minWidth: "20px" }}>
-                  <FiberManualRecordIcon sx={{ fontSize: "10px" }} color="primary" />
-                </ListItemIcon>
-                <ListItemText primary={tech} />
-              </ListItem>
-            ))}
-          </List>
-          <List className={styles.list}>
-            <Typography>
-              Backend:
-            </Typography>
-            {backend.map((tech, index) => (
-              <ListItem key={index} sx={{ padding: "0px", height: "20px" }}>
-                <ListItemIcon sx={{ minWidth: "20px" }}>
-                  <FiberManualRecordIcon sx={{ fontSize: "10px" }} color="primary" />
-                </ListItemIcon>
-                <ListItemText primary={tech} />
-              </ListItem>
-            ))}
-          </List>
-          <List className={styles.list}>
-            <Typography>
-              Frontend:
-            </Typography>
-            {frontend.map((tech, index) => (
-              <ListItem key={index} sx={{ padding: "0px", height: "20px" }}>
-                <ListItemIcon sx={{ minWidth: "20px" }}>
-                  <FiberManualRecordIcon sx={{ fontSize: "10px" }} color="primary" />
-                </ListItemIcon>
-                <ListItemText primary={tech} />
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-        
-      </Box>
-    </Box>
+    <Descriptions
+      period={period}
+      company={company}
+      position={position}
+      projects={projects}
+      summaries={summaries}
+      common={common}
+      backend={backend}
+      frontend={frontend}
+    />
   )
 }
 
-function GlobalX() {
+const GlobalX: React.FC = () => {
+  const period = "March 2024 - July 2024"
+  const company = new Map([["GlobalX PH", "http://globalx.com.ph/"]])
+  const position = "Fullstack Web and Game Developer"
+  const projects = new Map([
+    ["Karera", "https://dev.karera.live/"],
+  ])
+  
+  const summaries = [
+    "Developed system from scratch, focused on Zodiac Race",
+  ]
+  const common = [
+    "Typescript", "Jest", "Grafana"
+  ]
+  const backend = [
+    "NodeJS", "Fastify", "Uecs", "MySQL", "SQLite",
+  ]
+  const frontend = [
+    "ReactJS", "Mui", "Uecs", "Zustand", "Phaser", "PlayWright"
+  ]
   return (
-    <div className="timeline-block">
-      <div className='time'>
-        <span>March 2024 - July 2024</span>
-        <a href="https://globalx.com.ph/" target="_blank">GlobalX PH</a>
-        <span>Full Stack Web/Game Developer</span>
-      </div>
-      <div className="divider">
-        <div className="entry-dot"></div>
-      </div>
-      <div className="description">
-        <div>
-          <div>
-            <a href="https://dev.karera.live/" target="_blank">Karera</a>
-          </div>
-          - I was the lead developer of online betting Zodiac Race, focused on game and analytics
+    <Descriptions
+      period={period}
+      company={company}
+      position={position}
+      projects={projects}
+      summaries={summaries}
+      common={common}
+      backend={backend}
+      frontend={frontend}
+    />
+  )
+}
 
-          Their website is not up and the game, because last thing I heard is they are struggling on something.
-        </div>
-        <br></br>
-        Programming languages: Typescript
-        <br></br>
-        Tools: NodeJS, Fastify, ws WebSocket, ReactJS, MUI, Zustand, PlayWright, Jest, Grafana
-        Game Engine: Phaser, uecs
-      </div>
-    </div>
+const GameDevAlice: React.FC = () => {
+  const period = "Sept 2020 - Oct 2023"
+  const company = new Map([["GameDevAlice", "https://github.com/gamedevalice"]])
+  const position = "3D Game Developer"
+  const projects = new Map([
+    ["Ironverse", "/ironverse"],
+  ])
+  
+  const summaries = [
+    "Have built lots of proprietary prototypes, the link above is the only open-source we created using WASM.",
+  ]
+  const common = [
+    "Rust", "Javascript", "Bevy Engine", "Tokio", "Voxel", "Shader", "Multi-threading"
+  ]
+  const backend = [
+    "Cargo", "PostgreSQL", "Game Physics", "MySQL", "SQLite",
+  ]
+  const frontend = [
+    "WGSL", "WASM", "Web Worker"
+  ]
+  return (
+    <Descriptions
+      period={period}
+      company={company}
+      position={position}
+      projects={projects}
+      summaries={summaries}
+      common={common}
+      backend={backend}
+      frontend={frontend}
+    />
+  )
+}
+
+const Katanatechworks: React.FC = () => {
+  const period = "April 2017 - Sept 2020"
+  const company = new Map([["Katana Techworks Inc.", "https://www.katanatechworks.com/"]])
+  const position = "Web Developer"
+  const projects = new Map([
+    ["Muse Square", "https://musesquare.com/"],
+    ["PB Dionisio", "https://www.pbdionisio.com/shop/"],
+    ["TCC Inc.", "https://tccinc.com/"],
+  ])
+  
+  const summaries = [
+    "I learned a lot here about web development.",
+  ]
+  const common = [
+    "Javascript", "Wordpress", "Magento 2", "Postman", "Laravel", 
+  ]
+  const backend = [
+    "PHP", "MySQL/MariaDB"
+  ]
+  const frontend = [
+    "Cypress", "Google Analytics"
+  ]
+  return (
+    <Descriptions
+      period={period}
+      company={company}
+      position={position}
+      projects={projects}
+      summaries={summaries}
+      common={common}
+      backend={backend}
+      frontend={frontend}
+    />
+  )
+}
+
+const Hilsoft: React.FC = () => {
+  const period = "July 2016 - March 2017"
+  const company = new Map([["Hilsoft Inc.", "https://www.hilsoftinc.com/"]])
+  const position = "Java Developer"
+  const projects = new Map([
+    ["ERP System", ""],
+  ])
+  
+  const summaries = [
+    "Part of team developed ERP system development for CBTL and CWC",
+  ]
+  const common = [
+    "Java"
+  ]
+  const backend = [
+    "JEJB", "Struts", "MySQL", "Apache Ant"
+  ]
+  const frontend = [
+    "HTML", "Jaspersoft", "iReport"
+  ]
+  return (
+    <Descriptions
+      period={period}
+      company={company}
+      position={position}
+      projects={projects}
+      summaries={summaries}
+      common={common}
+      backend={backend}
+      frontend={frontend}
+    />
+  )
+}
+
+const Indigo: React.FC = () => {
+  const period = "Sept 2014 - July 2016"
+  const company = new Map([
+    ["Indigo.Entertainment Phils. Inc.", "https://ph.indeed.com/cmp/Indigo-Entertainment"]
+  ])
+  const position = "Game Developer"
+  const projects = new Map([
+    ["TMNT Deckd Out", "https://www.numuki.com/game/tmnt-deck-d-out/"],
+    ["Mr. Bean Skidding", "https://www.boomerangtv.co.uk/games/mr-bean-skidding"],
+    ["Alvin and the Chipmunks", "http://www.qgames.org/game/alvin-and-the-chipmunks-taking-center-stage.html"],
+    ["Nick Dance Machine", "https://www.numuki.com/game/nick-dance-machine/"],
+    ["Fairly Odd Parents", "https://www.numuki.com/game/fop-wishing-101/"],
+    ["PGBC Game Lad Blitz", "https://www.numuki.com/game/pgbc-game-lad-blitz/"],
+  ])
+  
+  const summaries = [
+    "I started my career as a Game Developer, we worked for Cartoon Network and Nickelodeon.",
+  ]
+  const common = [
+    "Haxe", "Flambe", "Mercurial", "Android"
+  ]
+  const backend = ["Java"]
+  const frontend = [
+    "HTML", "Flash", "OpenFL"
+  ]
+  return (
+    <Descriptions
+      period={period}
+      company={company}
+      position={position}
+      projects={projects}
+      summaries={summaries}
+      common={common}
+      backend={backend}
+      frontend={frontend}
+    />
   )
 }
 
